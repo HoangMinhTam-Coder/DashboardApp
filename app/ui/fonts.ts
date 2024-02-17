b3d29aea377f724ca5d8defd0250b591cfcb1884
DESKTOP-D8U1S3B\Admin <tam.minhhoang@infodation.vn>
2024-02-18 00:06:21 +0700

    Comit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lato, Lusitana, Poppins } from 'next/font/google';
+ 
+export const inter = Lato({ subsets: ['latin'], weight: "700" });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
